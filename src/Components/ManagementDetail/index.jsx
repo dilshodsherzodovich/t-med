e9d7f3766e52ab6afdf4b3bbd02c99bf57c11d95
dilshodmurodov <dilshod.sherzodovich.2004@gmail.com>
2024-11-28 12:27:43 +0500

    change: management page cards design changed

--- src/Components/ManagementDetail/index.jsx
+++ src/Components/ManagementDetail/index.jsx
@@ -4,58 +4,93 @@ import {
   MdMail,
   MdKeyboardArrowDown,
   MdKeyboardArrowUp,
+  MdReceipt,
 } from "react-icons/md";
 import "./ProfileCard.scss"; // Import SCSS file
 
 // ProfileCard component
-export default function ManagementDetail() {
+export default function ManagementDetail({ director }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="profile-card-wrapper d-flex justify-content-center align-items-center py-4">
-      <div className="card profile-card shadow-lg border-0 w-100">
+      <div className="card profile-card border-0 w-100">
         <div className="row g-0">
           {/* Profile Image Section */}
-          <div className="col-md-4 position-relative">
+          <div className="col-md-3 position-relative">
             <img
-              src="https://t-med.vercel.app/assets/img/management/MamasiddiqovMuxsinjonMusajonovich.jpg"
+              src={director?.image}
               alt="Profile photo"
               className="img-fluid w-100 h-100 object-cover rounded-start profile-image object-fit-cover"
+              style={{ objectPosition: "55% 45%" }}
             />
             <div className="overlay"></div>
           </div>
 
           {/* Profile Information Section */}
-          <div className="col-md-8 p-4">
-            <h2 className="h4 font-weight-semibold text-dark">
-              Rajabbayev Sharof Raximberdieyevich
-            </h2>
-            <p className="text-blue font-weight-medium">
-              Innovatsion rivojlanish agentligi direktori
-            </p>
+          <div className="col-md-9 p-4">
+            <div className="info-text">
+              <h2 className="h4 font-weight-semibold text-dark">
+                {director?.name}
+              </h2>
+              <p className="text-blue font-weight-medium">
+                {director?.position}
+              </p>
+            </div>
 
             <div className="mb-3">
-              <div className="d-flex align-items-center gap-3 text-dark">
-                <div className="bg-primary p-2 rounded-circle">
-                  <MdPhone className="h5 w-5 text-white" />
+              <div className="d-flex align-items-center gap-3 mb-1 ">
+                <div
+                  style={{
+                    background: "#dbeafe",
+                    color: "#2563eb",
+                    width: "30px",
+                    height: "30px",
+                  }}
+                  className="rounded-circle d-flex align-items-center justify-content-center "
+                >
+                  <MdPhone size={16} />
                 </div>
-                <span>+998 (71) 203-32-23</span>
-                <span className="text-blue-light font-weight-medium">
-                  (720)
+                <span style={{ color: "#374151" }}>
+                  {director?.phone_number}
                 </span>
               </div>
-              <div className="d-flex align-items-center gap-3 text-dark">
-                <div className="bg-primary p-2 rounded-circle">
-                  <MdMail className="h5 w-5 text-white" />
+              <div className="d-flex align-items-center gap-3 mb-1 ">
+                <div
+                  style={{
+                    background: "#dbeafe",
+                    color: "#2563eb",
+                    width: "30px",
+                    height: "30px",
+                  }}
+                  className="rounded-circle d-flex align-items-center justify-content-center "
+                >
+                  <MdMail size={16} />
                 </div>
-                <span>info@ilmiy.uz</span>
+                <span style={{ color: "#374151" }}>{director?.email}</span>
+              </div>
+              <div className="d-flex align-items-center gap-3">
+                <div
+                  style={{
+                    background: "#dbeafe",
+                    color: "#2563eb",
+                    width: "30px",
+                    height: "30px",
+                  }}
+                  className="rounded-circle d-flex align-items-center justify-content-center "
+                >
+                  <MdReceipt size={16} />
+                </div>
+                <span style={{ color: "#374151" }}>
+                  {director?.reception_days}
+                </span>
               </div>
             </div>
 
             {/* Toggle Button */}
             <button
               onClick={() => setIsOpen(!isOpen)}
-              className="btn btn-primary w-100 rounded-pill profile-btn"
+              className="btn btn-primary profile-btn"
             >
               <span>Batafsil</span>
               {isOpen ? (
@@ -71,18 +106,16 @@ export default function ManagementDetail() {
                 isOpen ? "open" : ""
               }`}
             >
-              <div className="bg-light p-3 rounded-3xl border mt-3">
-                <h3 className="h5 font-weight-semibold mb-2">
-                  Additional Information
-                </h3>
-                <p className="text-dark">
+              <div className=" px-2 rounded-3xl mt-3">
+                <h3 className="h5 font-weight-semibold mb-2">Tarjimai hol</h3>
+                <p className="">
                   Rajabbayev Sharof Raximberdieyevich is a distinguished leader
                   in the field of innovation and development. With years of
                   experience, he has been instrumental in driving
                   forward-thinking initiatives and fostering a culture of
                   innovation within the agency.
                 </p>
-                <ul className="mt-2 space-y-1 list-unstyled text-dark">
+                <ul className="mt-2 ">
                   <li>Expert in strategic planning and policy development</li>
                   <li>Advocate for technological advancement and research</li>
                   <li>
--- src/Pages/Management/index.jsx
+++ src/Pages/Management/index.jsx
@@ -9,78 +9,24 @@ const headingData = {
   title: "Rahbariyat",
 };
 
-const doctors = [
+const directors = [
   {
     name: "MAMASIDIKOV MUXSINJON MUSAJONOVICH",
-    subtitle: "Temir yo'l ijtimoiy xizmatlar muassasasi boshlig'i",
-    descriptionLabel: "Tarjimai hol",
-    description: [
-      `Mamasidikov Muxsinjon Mamajonovich 17.02.1976 yilda Farg‘ona vioyati Rishton tumanida tavallud topgan. Temir yo‘l soxasida 1994-yildan hozirgi vaqtgacha bir qator rahbarlik lavozimlarda ishlab kelmoqdalar. 2020-2023-yillarda “Sog‘liqni saqlash xizmati” boshlig‘i  lavozimida ishlaganlar. 2023-yildan hozirgi kunga qadar “Temir yo’l ijtimoiy xizmatlar” muassasasi boshlig‘I lavozimida faoliyat olib bormoqdalar. Kasbga bo‘lgan muxabbatlari va fidokorona mehnatlari uchun Faxriy temiryo‘lchi unvoni hamda “O‘zbekiston Respublikasi Sog‘liqni saqlash a’lochisi” ko‘krak nishoni bilan taqdirlanganlar.`,
-    ],
+    position: "Temir yo'l ijtimoiy xizmatlar muassasasi boshlig'i",
+    description: `Mamasidikov Muxsinjon Mamajonovich 17.02.1976 yilda Farg‘ona vioyati Rishton tumanida tavallud topgan. Temir yo‘l soxasida 1994-yildan hozirgi vaqtgacha bir qator rahbarlik lavozimlarda ishlab kelmoqdalar. 2020-2023-yillarda “Sog‘liqni saqlash xizmati” boshlig‘i  lavozimida ishlaganlar. 2023-yildan hozirgi kunga qadar “Temir yo’l ijtimoiy xizmatlar” muassasasi boshlig‘I lavozimida faoliyat olib bormoqdalar. Kasbga bo‘lgan muxabbatlari va fidokorona mehnatlari uchun Faxriy temiryo‘lchi unvoni hamda “O‘zbekiston Respublikasi Sog‘liqni saqlash a’lochisi” ko‘krak nishoni bilan taqdirlanganlar.`,
     image: "assets/img/management/MamasiddiqovMuxsinjonMusajonovich.jpg",
-    info: [
-      {
-        icon: <FaPhone width={28} height={28} />,
-        subtitle: "Telefon raqam",
-        title: "47-870",
-        secIcon: <IoIosMail width={28} height={28} />,
-        secSubtitle: "Email manzili",
-        secTitle: "nvs26@mail.ru",
-      },
-      {
-        icon: <FaReceipt width={28} height={28} />,
-        title: "Qabul kunlari",
-        subtitle: "Seshanba  soat 14-00 dan 16-00 gacha",
-      },
-    ],
+    phone_number: "99 001 02 04",
+    email: "",
+    reception_days: "",
   },
   {
     name: "Muxtarov Muzaffar Abdumuxitovich",
-    subtitle: "Muassasa boshlig‘i birinchi o‘rinbosari",
-    descriptionLabel: "Tarjimai hol",
-    description: [
-      `Muxtarov Muzaffar Abdumuxitovich 18.09.1977 yilda Toshkent shahrida tavallud topgan. 2001-yildan hozirgi vaqtgacha temir yo‘l sohasida bir qator rahbar lavozimlarida ishlab kelmoqdalar. 2023-yildan “Temir yo‘l ijtimoiy xizmarlar” muassasasida muassasa boshlig‘ining birinchi o‘rinbosari lavozimida faoliyat olib bormoqdalar. “Temir yo‘l ijtimoiy xizmatlar” muassasasida fidokorona mehnatlari uchun esdalik ko‘krak nishoni bilan taqdirlanganlar.`,
-    ],
+    position: "Muassasa boshlig‘i birinchi o‘rinbosari",
+    description: `Muxtarov Muzaffar Abdumuxitovich 18.09.1977 yilda Toshkent shahrida tavallud topgan. 2001-yildan hozirgi vaqtgacha temir yo‘l sohasida bir qator rahbar lavozimlarida ishlab kelmoqdalar. 2023-yildan “Temir yo‘l ijtimoiy xizmarlar” muassasasida muassasa boshlig‘ining birinchi o‘rinbosari lavozimida faoliyat olib bormoqdalar. “Temir yo‘l ijtimoiy xizmatlar” muassasasida fidokorona mehnatlari uchun esdalik ko‘krak nishoni bilan taqdirlanganlar.`,
     image: "assets/img/management/MuxtarovMuzaffarAbdumuxitovich.jpg",
-    info: [
-      {
-        icon: <FaPhone width={28} height={28} />,
-        subtitle: "Telefon raqam",
-        title: "47-870",
-        secIcon: <IoIosMail width={28} height={28} />,
-        secSubtitle: "Email manzili",
-        secTitle: "nvs26@mail.ru",
-      },
-      {
-        icon: <FaReceipt width={28} height={28} />,
-        title: "Qabul kunlari",
-        subtitle: "Chorshanba soat 14-00 dan 16-00 gacha",
-      },
-    ],
-  },
-  {
-    name: "Farmonov Ixtiyor Asrorovich",
-    subtitle: "Muassasa boshlig‘i o‘rinbosari",
-    descriptionLabel: "Tarjimai hol",
-    description: [
-      `Farmonov Ixtiyor Asrorovich 13.07.1970 yilda Buxoro viloyati Kogon shahrida tavallud topgan.  Temir yo‘l soxasida 1994 yildan hozirgi vaqtgacha bir qator rahbarlik lavozimlarda ishlab kelmoqdalar. 2024 yildan “Temir yo‘l ijtimoiy xizmatlar” muassasasi boshlig‘ining o‘rinbosari lavozimida faoliyat olib bormoqda.  Kasbga bo‘lgan muxabbatlari va fidokorona mehnatlari uchun Faxriy temiryo‘lchi unvoni bilan taqdirlangan.`,
-    ],
-    image: "assets/img/management/FarmonovIxtiyorAsrorovich.jpg",
-    info: [
-      {
-        icon: <FaPhone width={28} height={28} />,
-        subtitle: "Telefon raqam",
-        title: "47-870",
-        secIcon: <IoIosMail width={28} height={28} />,
-        secSubtitle: "Email manzili",
-        secTitle: "nvs26@mail.ru",
-      },
-      {
-        icon: <FaReceipt width={28} height={28} />,
-        title: "Qabul kunlari",
-        subtitle: "Payshanba soat 14-00 dan 16-00 gacha",
-      },
-    ],
+    phone_number: "99 001 02 04",
+    email: "",
+    reception_days: "",
   },
 ];
 
@@ -96,8 +42,8 @@ const Management = () => {
 
       <Section topSpaceLg="80" topSpaceMd="120">
         <div className="container">
-          {doctors?.map((item, index) => (
-            <ManagementDetail key={index} index={index} doctor={item} />
+          {directors?.map((item, index) => (
+            <ManagementDetail key={index} index={index} director={item} />
           ))}
         </div>
       </Section>
