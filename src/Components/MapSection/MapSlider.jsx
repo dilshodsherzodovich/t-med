143983e3e294984cd1cfae0fae3f211c419899b6
dilshodmurodov <dilshod.sherzodovich.2004@gmail.com>
2024-12-13 09:17:57 +0500

    fix: project optimized

--- src/Components/MapSection/MapSlider.jsx
+++ src/Components/MapSection/MapSlider.jsx
@@ -1,4 +1,5 @@
 import { Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const profiles = [
   {
@@ -25,20 +26,21 @@ const MapSlider = ({ sliders }) => {
                 <h2 className="card-title">{profile?.title}</h2>
               </div>
               <div className="card-body">
-                <div className="profile-info">
-                  <img
-                    src="https://innovation.gov.uz/media/CACHE/images/staff_images/Yakubov_Abdulaziz/12ed13de2eca29ed9458d2663beeff6e.JPG"
-                    alt={profile?.director?.fio}
-                    className="profile-image"
-                  />
-                  <div className="profile-details-map">
-                    <h3 className="profile-name">{profile?.director?.fio}</h3>
-                    <p className="profile-title">
-                      {profile?.director?.specialist}
-                    </p>
-                  </div>
-                </div>
                 <div className="contact-info">
+                  {profile?.organization_images?.length > 0 && (
+                    <img
+                      style={{
+                        width: "400px",
+                        height: "200px",
+                        objectFit: "cover",
+                        maxWidth: "100%",
+                        maxHeight: "auto",
+                        marginBottom: "0.5rem",
+                      }}
+                      src={profile?.organization_images[0].image}
+                      alt=""
+                    />
+                  )}
                   <div className="contact-item">
                     <i className="bi bi-telephone"></i>
                     <span>{profile?.director?.reception_number}</span>
@@ -51,6 +53,13 @@ const MapSlider = ({ sliders }) => {
                     <i className="bi bi-geo-alt"></i>
                     <span>{profile?.address}</span>
                   </div>
+                  <Link
+                    style={{ fontSize: "14px", paddingLeft: "0.5rem" }}
+                    className="text-primary"
+                    to={`institutions/${profile?.id}`}
+                  >
+                    Batafsil
+                  </Link>
                 </div>
               </div>
             </div>
--- src/Components/PageHeading/index.jsx
+++ src/Components/PageHeading/index.jsx
@@ -10,34 +10,36 @@ const PageHeading = ({ data, secondaryData }) => {
     setUrlSegments(pathSegments);
   }, []);
   return (
-    <div className="container">
-      <h1 className="cs_page_title" data-aos="fade-up">
-        {data?.title}
-      </h1>
-      <ol className="breadcrumb text-capitalize" data-aos="fade-right">
-        <li className="breadcrumb-item">
-          <Link to="/">Asosiy</Link>
-        </li>
-        {secondaryData ? (
-          <>
-            <li className="breadcrumb-item">
-              <span className="active"> {secondaryData}</span>
-            </li>
-          </>
-        ) : (
-          urlSegments.map((segment, index) => (
-            <li key={index} className="breadcrumb-item">
-              {index < urlSegments.length - 1 ? (
-                <Link to={`/${urlSegments.slice(0, index + 1).join("/")}`}>
-                  {segment}
-                </Link>
-              ) : (
-                <span className="active">{segment}</span>
-              )}
-            </li>
-          ))
-        )}
-      </ol>
+    <div className="overflow">
+      <div className="container">
+        <h1 className="cs_page_title" data-aos="fade-up">
+          {data?.title}
+        </h1>
+        <ol className="breadcrumb text-capitalize" data-aos="fade-right">
+          <li className="breadcrumb-item">
+            <Link to="/">Asosiy</Link>
+          </li>
+          {secondaryData ? (
+            <>
+              <li className="breadcrumb-item">
+                <span className="active"> {secondaryData}</span>
+              </li>
+            </>
+          ) : (
+            urlSegments.map((segment, index) => (
+              <li key={index} className="breadcrumb-item">
+                {index < urlSegments.length - 1 ? (
+                  <Link to={`/${urlSegments.slice(0, index + 1).join("/")}`}>
+                    {segment}
+                  </Link>
+                ) : (
+                  <span className="active">{segment}</span>
+                )}
+              </li>
+            ))
+          )}
+        </ol>
+      </div>
     </div>
   );
 };
