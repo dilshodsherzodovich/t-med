b3713a7a6372d209de15bc0321ffab1c2bbbcb30
justmirshod <mirshod.29.murodov.0804@gmail.com>
2024-12-13 08:46:31 +0500

    fix: 360 duplicated

--- src/Pages/Virtour/index.jsx
+++ src/Pages/Virtour/index.jsx
@@ -1,15 +1,47 @@
+import { useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+
 export default function Virtour() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (!searchParams.get("id")) {
+      setSearchParams({ id: 1 });
+    }
+  }, []);
+
+  const links = [
+    {
+      id: 1,
+      link: "https://kuula.co/share/collection/7ZjhB?logo=-1&info=0&fs=1&vr=0&sd=1&initload=1&thumbs=3&inst=0&keys=0",
+    },
+    {
+      id: 2,
+      link: "https://kuula.co/share/collection/7ZyXb?logo=-1&info=0&fs=1&vr=0&sd=1&initload=1&thumbs=3&inst=0&keys=0",
+    },
+    {
+      id: 3,
+      link: "https://kuula.co/share/collection/7ZyXK?logo=-1&info=0&fs=1&vr=0&sd=1&initload=0&thumbs=3&inst=0&keys=0",
+    },
+    {
+      id: 4,
+      link: "https://kuula.co/share/collection/7ZyXc?logo=-1&info=0&fs=1&vr=0&sd=1&initload=0&thumbs=3&inst=0&keys=0",
+    },
+  ];
+
   return (
-    <iframe
-      class="ku-embed"
-      frameborder="0"
-      allow="xr-spatial-tracking; gyroscope; accelerometer"
-      allowfullscreen
-      scrolling="no"
-      src="https://kuula.co/share/collection/7ZjhB?logo=-1&info=0&fs=1&vr=0&sd=1&initload=0&thumbs=3&inst=0&keys=0"
-      width='100%'
-      // height='100vh'
-      style={{height: "100vh"}}
-    ></iframe>
+    searchParams.get("id") && (
+      <iframe
+        class="ku-embed"
+        frameborder="0"
+        allow="xr-spatial-tracking; gyroscope; accelerometer"
+        allowfullscreen
+        scrolling="no"
+        src={links.find((link) => link?.id === +searchParams.get("id")).link}
+        width="100%"
+        // height='100vh'
+        style={{ height: "100vh" }}
+      ></iframe>
+    )
   );
 }
