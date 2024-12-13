8697d06bf1731fe1a3a06048f1af262b6925d7ea
justmirshod <mirshod.29.murodov.0804@gmail.com>
2024-12-13 05:41:05 +0500

    fix: rating api connected

--- src/Components/InstitutionDetails/SingleInstitution.jsx
+++ src/Components/InstitutionDetails/SingleInstitution.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import ReactStars from "react-rating-stars-component";
 import { FaCheckCircle } from "react-icons/fa";
 import DepartmentManager from "../Departments/DepartmentManager";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 function SingleInstitution({ data }) {
-  console.log(data);
+  const [rating, setRaiting] = useState(0);
+  const formRef = useRef();
+
+  const { id } = useParams();
+
+  const postData = async (data) => {
+    const response = await axios.post(
+      "https://nsuback.taskmanager.uz/reception/create-rating/",
+      data
+    );
+    return response.data;
+  };
+
+  const mutation = useMutation({
+    mutationFn: postData,
+    onSuccess: (data) => {
+      if (data?.id) {
+        formRef.current.reset();
+        setRaiting(0);
+        toast.success("Murojaatingiz muvaffaqiyatli yuborildi!", {
+          theme: "colored",
+        });
+      }
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    formData.append("organization", id);
+    formData.append("rating", rating);
+    mutation.mutate(formData);
+  };
 
   return (
     <div className="container">
@@ -28,6 +67,29 @@ function SingleInstitution({ data }) {
 
         <h3 className="cs_service_heading">Muassasa rahbari:</h3>
         <DepartmentManager data={data?.managers} />
+
+        <div className="cs_comment_section mt-5">
+          <h3 className="cs_service_heading">Tashkilot haqida fikringiz</h3>
+          <form action="" onSubmit={handleSubmit} ref={formRef}>
+            <textarea
+              name="reason"
+              id=""
+              rows={8}
+              className="w-100 border rounded-1 px-2 py-1"
+            ></textarea>
+            <ReactStars
+              count={5}
+              value={rating}
+              onChange={(value) => setRaiting(value)}
+              size="100px"
+            />
+            <div className="d-flex justify-content-end mt-2">
+              <button className="btn btn-dark" type="submit">
+                Jo'natish
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
